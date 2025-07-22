bae35670e744eb14c5d62225bd10abc4c724c6f3
FaceNach <ignacioijg@gmail.com>
2025-07-22 10:43:49 -0300

    Keep it going

---
+++ Ch3/go.mod
@@ -0,0 +1,3 @@
+module CompositeTypes
+
+go 1.24.4
---
+++ Ch3/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x = []int{1, 2, 3, 4}
+	var s = []int{1, 4: 5, 20, 35, 25: 25, 30, 40}
+	fmt.Println(x)
+	fmt.Println(s)
+
+}
